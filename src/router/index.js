916f1f91f3a8f11973d5a75cfaccc1c100e670d9
simon61610 <simon410379007@gmail.com>
2025-08-12 09:42:06 +0800

    [chore] 優化 router ，抽離 routes 陣列至獨立常數

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,51 @@
 import { createRouter, createWebHistory } from 'vue-router'
 //import Newpage from '../views/Newpage.vue'
 
+// routes 口訣：在哪個路徑下，render 哪個組件
+// {path: '路徑', component: 組件},
+const routes = [
+  // 這邊放 routes！
+
+
+
+
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes, // 等同 routes: routes
+})
+
+export default router
+
+
+// ----------------------- 以下放還沒確定好的 Routes，確定好再放到陣列裡 ------------------------
+// 另外要討論是否子物件要用 lazy-loading 的寫法
+
+
+/* 
+{
+  path: '/NewpageView',    // 在vue裡面 / = http://localhost:5173/ 也就是本機的意思
+  name: 'NewpageView',     //網頁的id
+  component: () => import('../views/NewpageView.vue'), //採用官方建議lazy-loaded(非同步載入)
+},
+
+{
+  path: '/article/:id',    // 在vue裡面 / = http://localhost:5173/ 也就是本機的意思
+  name: 'ArticleDetailView',     //網頁的id
+  component: () => import('../views/ArticleDetailView.vue'), //採用官方建議lazy-loaded(非同步載入)
+},
+*/
+
+
+// -----------------------------------------------
+
+
+/* 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-   /* {
-      path: '/NewpageView',    // 在vue裡面 / = http://localhost:5173/ 也就是本機的意思
-      name: 'NewpageView',     //網頁的id
-      component: () => import('../views/NewpageView.vue'), //採用官方建議lazy-loaded(非同步載入)
-    },
 
-    {
-      path: '/article/:id',    // 在vue裡面 / = http://localhost:5173/ 也就是本機的意思
-      name: 'ArticleDetailView',     //網頁的id
-      component: () => import('../views/ArticleDetailView.vue'), //採用官方建議lazy-loaded(非同步載入)
-    },*/
-    
     {
       path: '/about',
       name: 'about',
@@ -27,4 +57,5 @@ const router = createRouter({
   ],
 })
 
-export default router 
+export default router
+*/
