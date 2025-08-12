2c93ce5f0ce0cf17a52833aa0dd3b2a3d969b206
simon61610 <simon410379007@gmail.com>
2025-08-12 22:28:50 +0800

    [feat] 新增商城主頁

---
+++ src/components/shop/shophome/HeroBanner.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+
+</script>
+
+
+<template>
+    <!-- 用輪播套件，之後寫 -->
+    <div class="swiper">
+        輪播圖
+    </div>
+
+</template>
+
+
+<style scoped lang="scss">
+    @import '@/assets/styles/main.scss';
+    .swiper {
+        font-size: 100px;
+        font-weight: bold;
+        color: $primaryColor-800;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        width: 100%;
+        height: 840px;
+        outline: 1px solid red;
+    }
+
+</style>
