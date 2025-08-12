9458c5454eefc6fde8945d1660e6fcf788a72b59
Ani-TW <buzhubu98@gmail.com>
2025-08-12 10:13:50 +0800

    [組件] 地圖組件開發

--- src/components/starmap/MapReview.vue
+++ src/components/starmap/MapReview.vue
@@ -2,9 +2,9 @@
     <!-- 內容彈窗 -->
     <div class="map-reviewBox">
          <!-- 叉叉按鈕 -->
-        <img class="mapbox-close" src="../../assets/images/Logout.svg" alt="">
+        <img class="mapbox-close" src="../../assets/icons/icon-map-close.svg" alt="">
         <h3 class="mapbox-back">
-            <img class="mapbox-backTo" src="../../assets/images/Logout.svg" alt="">
+            <img class="mapbox-backTo" src="../../assets/icons/icon-map-goToBack.svg" alt="">
             返回上頁
         </h3>
         
@@ -14,7 +14,7 @@
             <div class="mapreview-title">
                 <h1>南瀛天文館</h1>
                 <h3>
-                    <img src="../../assets/logo.svg" alt="">
+                    <img src="../../assets/icons/icon-map-whitestar.svg" alt="">
                     4.7
                 </h3>
             </div>
@@ -43,11 +43,11 @@
                 <li>
                     <h4>Ani_98</h4>
                     <span class="review-score"> <!--到時候img 用v-for跑 評論幾分跑幾次-->
-                        <img src="../../assets/logo.svg" alt="星星">
-                        <img src="../../assets/logo.svg" alt="星星">
-                        <img src="../../assets/logo.svg" alt="星星">
-                        <img src="../../assets/logo.svg" alt="星星">
-                        <img src="../../assets/logo.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
                     </span>
                     <p class="cnContent--18px">
                         新手友善，剛好是天文館，可以先在天文館學習新知後，留到晚上用天文望遠鏡看星星。
@@ -59,11 +59,12 @@
                 <li>
                     <h4>Ani_98</h4>
                     <span class="review-score"> <!--到時候img 用v-for跑 評論幾分跑幾次-->
-                        <img src="../../assets/logo.svg" alt="星星">
-                        <img src="../../assets/logo.svg" alt="星星">
-                        <img src="../../assets/logo.svg" alt="星星">
-                        <img src="../../assets/logo.svg" alt="星星">
-                        <img src="../../assets/logo.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
+
                     </span>
                     <p class="cnContent--18px">
                         新手友善，剛好是天文館，可以先在天文館學習新知後，留到晚上用天文望遠鏡看星星。新手友善，剛好是天文館，可以先在天文館學習新知後，留到晚上用天文望遠鏡看星星。新手友善，剛好是天文館，可以先在天文館學習新知後，留到晚上用天文望遠鏡看星星。
@@ -75,11 +76,11 @@
                 <li>
                     <h4>Ani_98</h4>
                     <span class="review-score"> <!--到時候img 用v-for跑 評論幾分跑幾次-->
-                        <img src="../../assets/logo.svg" alt="星星">
-                        <img src="../../assets/logo.svg" alt="星星">
-                        <img src="../../assets/logo.svg" alt="星星">
-                        <img src="../../assets/logo.svg" alt="星星">
-                        <img src="../../assets/logo.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
                     </span>
                     <p class="cnContent--18px">
                         帶小孩來，光害少，車位是路邊白線，滿天星空真的美！
@@ -91,11 +92,11 @@
                 <li>
                     <h4>Ani_98</h4>
                     <span class="review-score"> <!--到時候img 用v-for跑 評論幾分跑幾次-->
-                        <img src="../../assets/logo.svg" alt="星星">
-                        <img src="../../assets/logo.svg" alt="星星">
-                        <img src="../../assets/logo.svg" alt="星星">
-                        <img src="../../assets/logo.svg" alt="星星">
-                        <img src="../../assets/logo.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
+                        <img src="../../assets/icons/icon-filledStar.svg" alt="星星">
                     </span>
                     <p class="cnContent--18px">
                         新手友善，剛好是天文館，可以先在天文館學習新知後，留到晚上用天文望遠鏡看星星。
@@ -110,12 +111,28 @@
         </div>
    
    </div>
+
+    
 </template>
 
 <style scoped lang="scss">
 @import '../../assets/styles/main.scss';
-    
-    //評論列表
+
+//評論攔
+.mapreview-writePlace{
+    position: fixed;
+    z-index: 100;
+
+    border: 2px solid $primaryColor-100;
+    width: 430px;
+    height: 680px;
+    background-color: #ffffff;
+    border-radius: 20px;
+    box-sizing: border-box;
+}
+
+
+//評論列表
 .mapreview-list{
     display: flex;
     flex-direction: column;
@@ -124,6 +141,7 @@
     border-top: 1px solid $primaryColor-100;
 
     overflow-y: auto;
+    
 }   
 .mapreview-list li{
     display: flex;
@@ -199,6 +217,8 @@
 }
 .mapbox-backTo{
     height: 32px;
+    background-color: #ffffff;
+    border-radius: 999px;
 }
 
 
@@ -234,15 +254,18 @@
     line-height: 120%;
 }
 .mapreview-title h3{
-    // border: 1px solid red;
     align-items: center;
+
+    display: flex;
+    align-items: center;
+    gap: 4px;
 
     font-size: $pcChFont-H3;
     font-weight: bold;
 }
 
 .mapreview-title img{
-    width: 24px;
+    width: 36px;
 }
 
 
---
+++ src/components/starmap/ReviewPlace.vue
@@ -0,0 +1,31 @@
+<template>
+    <!-- 評論攔 -->
+    <div class="mapreview-writePlace">
+        <h3>會員名稱</h3>
+        <div class="review-writePlace-score">
+            <img src="../../assets/icons/icon-borderStar.svg" alt="空星星">
+            <img src="../../assets/icons/icon-borderStar.svg" alt="空星星">
+            <img src="../../assets/icons/icon-borderStar.svg" alt="空星星">
+            <img src="../../assets/icons/icon-borderStar.svg" alt="空星星">
+            <img src="../../assets/icons/icon-borderStar.svg" alt="空星星">
+        </div>
+        <div class="review-writePlace-text">
+            <h5>*說出你的想法：</h5>
+            <textarea name="" id=""></textarea>
+            <p>字數：0/300</p>
+        </div>
+        <div class="review-writePlace-photo">
+            <h5>上傳照片：</h5>
+            <input type="file">
+        </div>
+        <div>
+            <button class="cancel">取消</button>
+            <button class="submit">張貼</button>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@import '../../assets/styles/main.scss';
+
+</style>
