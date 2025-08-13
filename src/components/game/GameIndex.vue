7c11dc7e6fdca3e9c4b2674a99618fa567f901d2
Nadia.chang <balapu131313@gmail.com>
2025-08-13 20:05:56 +0800

    [feat] 新增遊戲組件

---
+++ src/components/game/GameIndex.vue
@@ -0,0 +1,23 @@
+<template> 
+    <div class="test-box">這是測試元件</div>
+	<input type="text">這是input
+</template> 
+
+
+<script setup>//放js
+
+</script>
+
+<style scoped lang="scss">//放css
+@import '@/assets/styles/main';
+
+.test-box {
+	color: $secondaryColor-orange;
+}
+input {
+	border: 1px solid $inputColor-border;
+}
+input:focus {
+	outline: 1px solid $inputColor-focus;
+}
+</style>
