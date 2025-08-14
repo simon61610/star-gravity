67bdaf0a7de5d3f43e519a533a555478065575a6
Ani-TW <buzhubu98@gmail.com>
2025-08-14 23:19:24 +0800

    [feat] 新建router所需的地圖page

---
+++ src/views/map/MapFirstPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="wrapper">
+        <h1 class="map-title">精選觀星地圖</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.map-title{
+    font-size: 60px;
+}
+.wrapper{
+    background-image: url(../../assets/images/map/map-first_bg.svg);
+    width: 100%;
+    height: calc( 100vh - 50px);
+}
+</style>
---
+++ src/views/map/MapMainPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="wrapper">
+        <!-- 列表與地圖 -->
+        <MapMain />
+
+        <!-- 大彈窗 -->
+        <!-- <MapDetail/> -->
+
+        <!-- 地點評論 -->
+        <!-- <MapReview/> -->
+
+        <!-- 評論填寫框 -->
+        <ReviewPlace/>
+
+        
+    </div>
+</template>
+
+<script setup>
+import MapMain from '@/components/starmap/MapMain.vue';
+import MapDetail from '@/components/starmap/MapDetail.vue';
+import MapReview from '@/components/starmap/MapReview.vue';
+import ReviewPlace from '@/components/starmap/ReviewPlace.vue';
+</script>
