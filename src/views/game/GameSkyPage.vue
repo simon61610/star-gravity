618451ea67e21366048978cd88a38f5bb11b97c8
Nadia.chang <balapu131313@gmail.com>
2025-08-16 01:38:27 +0800

    [feat] 新增遊戲頁面

--- src/views/game/GameSkyPage.vue
+++ src/views/game/GameSkyPage.vue
@@ -1,1 +1,17 @@
-<template></template>
+<template>
+  <div class="game">
+    <div class="game-type">
+      <h1></h1>
+      <div class="cards">
+        <div class="card">
+              <img src="" alt="">
+              <span></span>
+          </div>
+          <div class="card">
+             <img src="" alt="">
+              <span></span>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
