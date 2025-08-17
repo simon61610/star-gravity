e529b04385befa7a2fd17a309d848bc5eaf1838e
simon61610 <simon410379007@gmail.com>
2025-08-17 22:45:27 +0800

    <feat> 新增購物車進度條功能

---
+++ src/components/shop/cart/CheckoutStepper.vue
@@ -0,0 +1,91 @@
+<script setup>
+    import { computed } from 'vue'
+
+    // 接收父組件屬性與屬性值
+    const props = defineProps(['steps', 'current'])
+
+    // 計算寬度幾趴
+    const percent = computed(() => 
+        ( props.current / props.steps.length ) * 100
+    )
+
+</script>
+
+
+<template>
+    <section class="checkout-stepper">
+        <!-- 步驟 -->
+        <div class="steps">
+            <div class="step" v-for="(title, index) in steps">
+                <div class="step-number" :class="{ active: index + 1 <= current}">{{ index + 1 }}</div>
+                <p class="step-title">{{ title }}</p>
+            </div>
+        </div>
+
+        <!-- 步驟條 -->
+        <div class="progress">
+            <div class="bar" :style="{ width: percent + '%'}"></div>
+        </div>
+    </section>
+
+
+
+
+</template>
+
+
+
+<style scoped lang="scss">
+    @import '@/assets/styles/main.scss';
+
+    .checkout-stepper {
+
+        // ----- 步驟 -----
+        .steps {
+            display: flex;
+            justify-content: space-around;
+            margin-bottom: 24px;
+            .step {
+                color: $primaryColor-500;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                gap: 12px;
+                .step-number {
+                    font-size: 16px;
+                    font-weight: bold;
+                    border: 3px solid $primaryColor-500;
+                    border-radius: 50%;
+                    width: 36px;
+                    height: 36px;
+                    aspect-ratio: 1 / 1;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                }
+                .step-number.active { // 用屬性值 trus false 判斷是否加上
+                    border: 3px solid $primaryColor-900;
+                    color: $primaryColor-900;
+                }
+                .step-title {}
+            }
+        }
+
+        // ----- 步驟條 -----
+        .progress {
+            width: 100%;
+            height: 12px;
+            border-radius: 999px;
+            background-color: #ccc;
+            overflow: hidden; // 遮住進度條
+
+            .bar {
+                height: 100%;
+                background-color: $primaryColor-900;
+                border-radius: 999px;
+            }
+        }
+    }
+
+
+</style>
--- src/views/shop/CartCheckoutPage.vue
+++ src/views/shop/CartCheckoutPage.vue
@@ -1,1 +1,47 @@
-<template></template>
+<script setup>
+
+import { ref } from 'vue'
+
+// 組件
+import CartBanner from '@/components/shop/cart/CartBanner.vue';
+import CheckoutStepper from '@/components/shop/cart/CheckoutStepper.vue';
+
+// 進度條: 傳屬性值讓子組件接收並套用
+const step = ref(1) // 目前步驟
+const steps = ['準備結帳', '填寫資料', '完成訂單'] // 步驟內容
+
+</script>
+
+
+<template>
+    <CartBanner />
+    <section class="container">
+        <div>
+            <CheckoutStepper :current="step" :steps="steps"/>
+        </div>
+
+
+
+
+
+
+
+
+    </section>
+</template>
+
+
+<style scoped lang="scss">
+@import '@/assets/styles/main.scss';
+
+    .container {
+        max-width: 1200px;
+        margin: 0 auto;
+        padding: 40px 16px;
+
+
+
+    }
+
+
+</style>
