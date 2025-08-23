bdd2904edf42ea520de6f2e4b4906b8f95ec22e4
simon61610 <simon410379007@gmail.com>
2025-08-23 11:23:57 +0800

    [feat] 新增購物車 storage 資料儲存功能

--- src/components/shop/category/ProductList.vue
+++ src/components/shop/category/ProductList.vue
@@ -56,26 +56,95 @@
         const start = (currentPage.value - 1) * pageSize.value // 從第幾筆開始
         return filteredItems.value.slice(start, start + pageSize.value) // 顯示的商品陣列
     })
+
+
+
+    // =====================================================
+    // ==================== 加入購物車 ====================
+    // =====================================================
+    
+    const storage = localStorage
+
+    function addCart(product){
+        if(!storage['addItemList']){
+            storage['addItemList'] = ''
+        }
+
+        /* Storage 的 Key 和 Value */
+        const itemId = `P${product.id}`
+        const itemValue = `${product.name}|${product.pic}|${product.specialPrice}`
+
+        // alert(itemValue) 
+        // itemId => P1 
+        // itemValue => 基礎入門型 商品 1|https://placehold.co/480x480?text=Product+1|9718
+        // ===============================================================================
+
+        /* 直接從物件中抓的資料字串 */
+        const itemName = product.name // 基礎入門型 商品 1
+        const itemPic = product.pic // https://placehold.co/480x480?text=Product+1
+        const itemSpecialPrice = product.specialPrice // 9718
+        // alert(itemSpecialPrice)
+
+        // ===============================================================================
+        
+        if(storage[itemId]){ // 如果已經買過，價格累加
+            let existInfo = storage[itemId].split('|') // 字串切割成陣列，用 split 方法
+            // console.log(existInfo) ['基礎入門型 商品 1', 'https://placehold.co/480x480?text=Product+1', '9718']
+            let existSpecialPrice = parseInt(existInfo[2]) // 原本的價格
+            let newExistPrice = existSpecialPrice + itemSpecialPrice
+            let updatedInfo = `${existInfo[0]}|${existInfo[1]}|${newExistPrice}`
+            // alert(updatedInfo)
+
+            storage[itemId] = updatedInfo
+
+        }else{ //第一次買
+            storage['addItemList'] += `${itemId}, `
+            storage[itemId] = itemValue
+        }
+    }
+
+
+
+
+
+
 </script>
+
+
+
 
 <template>
     <section>
+        <!-- ----------------------- 購物車區塊 ----------------------- -->
+        <!-- <section class="cart">
+            <h1>這是購物車</h1>
+            <div class="cartbox">
+                <h2>商品名稱：</h2>
+                <p>商品數量：</p>
+                <p>商品金額：</p>
+                <h2>總計數量：</h2>
+                <h2>總計金額：</h2>
+            </div>
+        </section> -->
+        <!-- ----------------------- 購物車區塊 ----------------------- -->
+
+
         <div class="product-items">
-            <RouterLink 
-             :to="`/product/${item.id}`"
-             class="item__card router-link" 
-             v-for="( item, index ) in showItems"
-            > 
-            <!-- 用顯示的商品陣列跑 v-for -->
+            <div class="item__card" v-for="( item, index ) in showItems"> <!-- 用顯示的商品陣列跑 v-for -->
+
+                <RouterLink :to="`/product/${item.id}`"class="router-link" > 
                 <img :src="item.pic" alt="商品假圖" class="item__card__img">
                 <div class="item__card__text">
                     <h1 class="item__card__text--name">{{ item.name }}</h1>
-                    <h2 class="item__card__text--price">NT$ {{ item.price }}</h2>
-                    <div class="item__card__text--cart-btn">
-                        <i class="fa-solid fa-cart-shopping"></i>
-                    </div>
+                    <h2 class="item__card__text--spe-price">NT$ {{ item.specialPrice }}</h2>
+                    <h3 class="item__card__text--price">NT$ {{ item.price }}</h3>
                 </div>
-            </RouterLink>
+                </RouterLink>
+
+                <div class="item__card--cart-btn" @click="addCart(item)">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </div>
+            </div>
         </div>
         <div>  
             <Pagination
@@ -124,23 +193,30 @@
                     overflow: hidden;
                     text-overflow: ellipsis;
                 }
-                &--price {
+                &--spe-price {
                     font-size: $pcChFont-p;
+                    margin-bottom: 12px;
+                }
+
+                &--price {
+                    color: #ccc;
+                    text-decoration: line-through;
                     margin-bottom: 16px;
                 }
-                // 購物車按鈕
-                &--cart-btn {
-                    border: 2px solid white;
-                    border-radius: 8px;
-                    font-size: 16px;
-                    padding: 6px 0;
-                    transition: all 0.3s ease;
+            }
+            
+            // 購物車按鈕
+            &--cart-btn {
+                border: 2px solid white;
+                border-radius: 8px;
+                font-size: 16px;
+                padding: 6px 0;
+                transition: all 0.3s ease;
 
-                    // 購物車按鈕 hover 效果
-                    &:hover {
-                        border: 2px solid $primaryColor-500;
-                        color: $primaryColor-500;
-                    }
+                // 購物車按鈕 hover 效果
+                &:hover {
+                    border: 2px solid $primaryColor-500;
+                    color: $primaryColor-500;
                 }
             }
         }
