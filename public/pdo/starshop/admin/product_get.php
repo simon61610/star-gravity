34188e9c5e9944823c4f24803555074677b03c4b
simon61610 <simon410379007@gmail.com>
2025-09-06 12:52:57 +0800

    [feat] 修改上架商品 php

---
+++ public/pdo/starshop/admin/product_get.php
@@ -0,0 +1,22 @@
+<?php
+
+
+    //建立PDO物件，並放入指定的相關資料
+    include('../../pdo.php');
+
+    //建立SQL
+    $sql = "SELECT * FROM Product;";
+
+    //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
+    $statement = $pdo -> query($sql);
+
+    //抓出全部且依照順序封裝成一個二維陣列
+    $data = $statement -> fetchAll();
+
+    echo json_encode($data);
+
+
+
+
+
+?>
