92d7989368448b4c5f556506be6447e3af2ebcc5
simon61610 <simon410379007@gmail.com>
2025-09-14 13:18:22 +0800

    [feat] 新增 public 資料夾環境變數路徑

--- public/pdo/cors.php
+++ public/pdo/cors.php
@@ -1,6 +1,7 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://localhost:5173");
+// header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, X-Requested-With, Authorization");
 header("Access-Control-Allow-Credentials: true");
