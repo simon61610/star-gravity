85710d21853ec1e4f343877b05b558095539b029
chenyu-66 <melody19990607@gmail.com>
2025-09-15 22:53:48 +0800

    [feat] 新增活動選染

--- public/pdo/Member/getMemberEvent.php
+++ public/pdo/Member/getMemberEvent.php
@@ -2,7 +2,14 @@
 
 include '../pdo.php';
 
-$data = json_decode(file_get_contents("php://input"), true) ;
+$data = json_decode(file_get_contents("php://input"), true);
+
+
+// echo 123;
+// 
+// echo json_encode([
+//     "message" => "Hello"
+// ]);
 
 $memberId = (int)($data['memberId'] ?? 0);
 
@@ -12,25 +19,34 @@ if($memberId <= 0) {
 };
 
 // 建立sql
-$sql = "SELECT event_name, 
-        DATE_FORMAT(event_start, '%Y-%m-%d %H:%i') AS event_date,
-        event_place, event_status
-        FROM `Event`
-        ORDER BY ID DESC";
+$sql = "SELECT j.* , e.event_name, e.event_place, e.event_status
+        from Joiner j
+        join Event e on j.event_id = e.ID 
+        WHERE j.member_id = :memberId  
+        ";
+
 $statement = $pdo->prepare($sql);
-$statement->bindValue(':memberId', $data["memberId"]);
-$statement->execute();
+$statement->bindValue(':memberId', $memberId, PDO::PARAM_INT);
+// $statement->bindValue(':memberId', $data["memberId"]);
+$result = $statement->execute();
+
+// echo $result;
 
 $events = $statement->fetchAll();
+// $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// 格式化輸出
+// print_r($events);
+
+//格式化輸出
 $out = [];
 foreach ($events as $e) {
     $out[] = [
         'event_name' => $e['event_name'],
-        'event_date'   => $e['event_date'],
+        // 'event_date'   => $e['event_date'],
         'event_place'  => $e['event_place'],
-        'event_status' => $e['event_status']
+        'event_status' => $e['event_status'],
+        'registration_number' => $e['registration_number'],
+        'registration_date' => $e['registration_date']
     ];
 };
 
