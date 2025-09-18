5b058eb3425464ff74aa52b7ff66cae2c09ce07b
cks007x <s86481215@gmail.com>
2025-09-18 17:15:34 +0800

    [feat]新增游標樣式

--- src/views/admin/AdminLayoutPage.vue
+++ src/views/admin/AdminLayoutPage.vue
@@ -1,23 +1,31 @@
 <script setup>
     import {ref} from 'vue'
+    import { onMounted, onBeforeUnmount } from "vue"
     import AdminHeader from '@/components/admin/AdminHeader.vue';
     import AdminSidebar from '@/components/admin/AdminSidebar.vue';
+
+    onMounted(() => {
+        document.body.classList.add("admin-layout")
+        console.log("AdminLayout mounted")
+        })
+
+        onBeforeUnmount(() => {
+        document.body.classList.remove("admin-layout")
+    })
 </script>
 <template>
-
-   <div class="Admin-home-wapper">
-        <header>
-            <AdminHeader/>
-        </header>
-        
-        
-        <main class="Admin-home-box">
-            <router-view/>
-            <AdminSidebar/>
-        </main>
  
-    </div>
-
+        <div class="Admin-home-wapper">
+            <header>
+                <AdminHeader/>
+            </header>
+            <main class="Admin-home-box">
+                <router-view/>
+                <AdminSidebar/>
+            </main>
+        
+        </div>
+  
 </template>
 
 <style scoped>
