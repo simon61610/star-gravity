8cd24354c8ec539340de632192b4a8cd1e462b4f
Ani-TW <buzhubu98@gmail.com>
2025-09-19 19:47:17 +0800

    [refactor] leaftlet地圖套件RWD調整

--- public/pdo/map/getLocationList.php
+++ public/pdo/map/getLocationList.php
@@ -19,29 +19,29 @@ echo json_encode($list);
 
 //將資料庫原有的分數統一算平均值並塞回資料庫
 //建立SQL
-for( $i=1 ; $i<20 ; $i++){
-    $id = $i ;
-    $sqlScore = " SELECT score from Review
-                  where location_id = :id " ;
-    $statement2 = $pdo->prepare( $sqlScore);
-    $statement2 ->bindValue(":id" , $id);
-    $statement2 ->execute();
+// for( $i=1 ; $i<20 ; $i++){
+//     $id = $i ;
+//     $sqlScore = " SELECT score from Review
+//                   where location_id = :id " ;
+//     $statement2 = $pdo->prepare( $sqlScore);
+//     $statement2 ->bindValue(":id" , $id);
+//     $statement2 ->execute();
 
-    $scoreList = $statement2->fetchAll();
-    $scoreSum = 0;
-    for( $j = 0; $j < count($scoreList); $j++ ){
-        $scoreSum += intval($scoreList[$j]['score']);
-    };
-    $scoreAverage =  round( $scoreSum / count($scoreList) , 1 ) ;
-    echo $scoreAverage;
+//     $scoreList = $statement2->fetchAll();
+//     $scoreSum = 0;
+//     for( $j = 0; $j < count($scoreList); $j++ ){
+//         $scoreSum += intval($scoreList[$j]['score']);
+//     };
+//     $scoreAverage =  round( $scoreSum / count($scoreList) , 1 ) ;
+//     echo $scoreAverage;
 
-    //把平均值塞回去該地點的資料庫
-    $sqlSc = " UPDATE Location SET score = :scoreAverage
-            WHERE ID = :id " ;
-    $statement3 = $pdo->prepare( $sqlSc);
-    $statement3 ->bindValue(":scoreAverage" , $scoreAverage);
-    $statement3 ->bindValue(":id" , $id);
-    $statement3 ->execute();
-};
+//     //把平均值塞回去該地點的資料庫
+//     $sqlSc = " UPDATE Location SET score = :scoreAverage
+//             WHERE ID = :id " ;
+//     $statement3 = $pdo->prepare( $sqlSc);
+//     $statement3 ->bindValue(":scoreAverage" , $scoreAverage);
+//     $statement3 ->bindValue(":id" , $id);
+//     $statement3 ->execute();
+// };
 
 ?>
